7b3212e4e3409e690466878684b7c056d8671501
ArickCoder <arickff02@gmail.com>
2024-11-22 22:02:43 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Tree extends React.Component {
+    render() {
+        return <h2>This is my Tree garden.</h2>
+    }
+}
+
+export default Tree;
---
+++ index3.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tree from './app.js';
+
+ReactDOM.render(<Student />, document.getElementById('root'));
